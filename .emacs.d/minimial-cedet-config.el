82c6050b36b097e62ee694290b06c50439444495
CodeDiablos <codediablos@gmail.com>
2016-12-08 15:19:19 +0800

    add more semantic modes

--- .emacs.d/minimial-cedet-config.el
+++ .emacs.d/minimial-cedet-config.el
@@ -14,12 +14,15 @@
 ;;(add-to-list 'load-path (concat cedet-root-path "contrib"))
 
 ;; select which submodes we want to activate
-(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
-(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
-;;(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
-;;(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+;;(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode)
 
 ;; Activate semantic
 (semantic-mode 1)
